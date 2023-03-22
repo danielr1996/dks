03de634775382c1ea21889e8b5045a6b9b4e49dc
Daniel Richter <danielrichter@posteo.de>
2023-03-22 23:02:53 +0100

    feat: add haproxy ingress controller
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

---
+++ platform/ingress/nginx.ts
@@ -0,0 +1,44 @@
+import * as k8s from "@pulumi/kubernetes";
+
+new k8s.helm.v3.Release("nginxpublic", {
+    chart: "nginx",
+    version: "13.2.22",
+    namespace: "default",
+    name: 'nginxpublic',
+    createNamespace: true,
+    repositoryOpts: {
+        repo: "https://charts.bitnami.com/bitnami",
+    },
+    values: {
+        ingress: {
+            tls: true,
+            enabled: true,
+            hostname: 'nginx.danielrichter.codes',
+            ingressClassName: 'public',
+            annotations: {
+                "cert-manager.io/cluster-issuer": "letsencrypt-prod"
+            }
+        }
+    }
+});
+new k8s.helm.v3.Release("nginxprivate", {
+    chart: "nginx",
+    version: "13.2.22",
+    namespace: "default",
+    name: 'nginxprivate',
+    createNamespace: true,
+    repositoryOpts: {
+        repo: "https://charts.bitnami.com/bitnami",
+    },
+    values: {
+        ingress: {
+            tls: true,
+            enabled: true,
+            hostname: 'nginx.dani.rip',
+            ingressClassName: 'private',
+            annotations: {
+                "cert-manager.io/cluster-issuer": "letsencrypt-prod"
+            }
+        }
+    }
+});
