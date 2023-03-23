9fb43c7a006783eaca8a174e825e93acd9b3d99b
Daniel Richter <danielrichter@posteo.de>
2023-03-23 20:25:52 +0100

    feat: configure backup
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- platform/storage/storage.ts
+++ platform/storage/storage.ts
@@ -1,6 +1,17 @@
 import * as k8s from "@pulumi/kubernetes";
+import {credentials} from "../credentials"
 
 const namespace = 'longhorn-system'
+
+const secret = new k8s.core.v1.Secret('longhorn-backup-secret',{
+    metadata: {
+        name: 'longhorn-backup-secret',
+        namespace,
+    },
+    type: 'Opaque',
+    stringData: credentials.storage.credentials
+})
+
 const longhorn = new k8s.helm.v3.Release("storage", {
     chart: "longhorn",
     version: "1.4.0",
@@ -23,6 +34,10 @@ const longhorn = new k8s.helm.v3.Release("storage", {
             annotations: {
                 "cert-manager.io/cluster-issuer": "letsencrypt-prod"
             }
+        },
+        defaultSettings: {
+            backupTarget: credentials.storage.backupTarget,
+            backupTargetCredentialSecret: secret.metadata.name
         }
     }
 });
@@ -31,14 +46,31 @@ new k8s.apiextensions.CustomResource('snapshotjob', {
     apiVersion: 'longhorn.io/v1beta1',
     kind: 'RecurringJob',
     metadata: {
-        name: 'default-nightly-snapshot',
+        name: 'default-hourly-snapshot',
         namespace
     },
     spec: {
-        cron: '20 4 * * ?',
+        cron: '0 * * * *',
         task: 'snapshot',
         groups: ['default'],
         retain: 3,
+        concurrency: 2,
+    }
+}, {dependsOn: longhorn})
+
+new k8s.apiextensions.CustomResource('backupjob', {
+    apiVersion: 'longhorn.io/v1beta1',
+    kind: 'RecurringJob',
+    metadata: {
+        name: 'default-nightly-backup',
+        namespace
+    },
+    spec: {
+        // cron: '20 4 * * ?',
+        cron: '20 * * * *',
+        task: 'backup',
+        groups: ['default'],
+        retain: 10,
         concurrency: 2,
     }
 }, {dependsOn: longhorn})
